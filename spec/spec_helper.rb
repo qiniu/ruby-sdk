9f07e8d23679f2e81f50f84fdd18869e361ca944
404 <awhy.xu@gmail.com>
2012-08-14 18:02:05 +0800

    added watermarking and all tests passed (staging env)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,11 @@ require 'rspec'
 
 RSpec.configure do |config|
   config.before :all do
-#=begin
+=begin
     Qiniu::RS.establish_connection! :access_key => "3fPHl_SLkPXdioqI_A8_NGngPWVJhlDk2ktRjogH",
                                     :secret_key => "bXTPMDJrVYRJUiSDRFtFYwycVD_mjXxYWrCYlDHy"
-#=end
-=begin
+=end
+#=begin
     Qiniu::RS.establish_connection! :access_key => "bE21M6FW9V7zAFrBY5psgKOKJQLiBj12qMWTpc57",
                                     :secret_key => "uMo7Nyq_eDK_CuQ8_FYCxoTHQZqjiaPh-cbiKO7L",
                                     :auth_url   => "http://m1.qbox.me:13001/oauth2/token",
@@ -19,16 +19,6 @@ RSpec.configure do |config|
                                     :up_host    => "http://m1.qbox.me:13019",
                                     :pub_host   => "http://m1.qbox.me:13012",
                                     :eu_host    => "http://m1.qbox.me:13050"
-=end
-=begin
-    Qiniu::RS.establish_connection! :access_key => "k6N9zXGKUs7UFmJtPXLWOF4idSAgPL4xA6BApBd-",
-                                    :secret_key => "p77h4hLERjGPi1Aw4P_G5qHGMKcz1MeSz3CqnYMV",
-                                    :auth_url   => "http://127.0.0.1:9100/oauth2/token",
-                                    :rs_host    => "http://127.0.0.1:9400",
-                                    :io_host    => "http://127.0.0.1:9200",
-                                    :up_host    => "http://127.0.0.1:11200",
-                                    :pub_host   => "http://127.0.0.1:10200",
-                                    :eu_host    => "http://127.0.0.1:15000"
-=end
+#=end
   end
 end
