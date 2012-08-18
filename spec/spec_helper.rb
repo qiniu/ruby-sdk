7b628281d32f4022a967b60b03ba6bc13e97a42d
404 <awhy.xu@gmail.com>
2012-08-19 02:03:23 +0800

    changed test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,6 @@ require 'rspec'
 
 RSpec.configure do |config|
   config.before :all do
-#=begin
-    Qiniu::RS.establish_connection! :access_key => "3fPHl_SLkPXdioqI_A8_NGngPWVJhlDk2ktRjogH",
-                                    :secret_key => "bXTPMDJrVYRJUiSDRFtFYwycVD_mjXxYWrCYlDHy"
-#=end
-=begin
     Qiniu::RS.establish_connection! :access_key => "bE21M6FW9V7zAFrBY5psgKOKJQLiBj12qMWTpc57",
                                     :secret_key => "uMo7Nyq_eDK_CuQ8_FYCxoTHQZqjiaPh-cbiKO7L",
                                     :auth_url   => "http://m1.qbox.me:13001/oauth2/token",
@@ -19,6 +14,5 @@ RSpec.configure do |config|
                                     :up_host    => "http://m1.qbox.me:13019",
                                     :pub_host   => "http://m1.qbox.me:13012",
                                     :eu_host    => "http://m1.qbox.me:13050"
-=end
   end
 end
