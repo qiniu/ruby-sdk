0b50a40d7c0c1ebb96b818b52fe94a4031699bb0
404 <awhy.xu@gmail.com>
2012-11-06 23:19:14 +0800

    v3.2.0

--- lib/qiniu/rs/version.rb
+++ lib/qiniu/rs/version.rb
@@ -4,8 +4,8 @@ module Qiniu
   module RS
     module Version
       MAJOR = 3
-      MINOR = 1
-      PATCH = 1
+      MINOR = 2
+      PATCH = 0
       # Returns a version string by joining <tt>MAJOR</tt>, <tt>MINOR</tt>, and <tt>PATCH</tt> with <tt>'.'</tt>
       #
       # Example
