5a64650c3cb9d42194ba35e6635d4f0519554d80
soffolk <zlx.star@gmail.com>
2013-09-29 20:57:00 +0800

    Add LOAD_PATH to reduce duplicate require code

--- lib/qiniu-rs.rb
+++ lib/qiniu-rs.rb
@@ -1,8 +1,8 @@
 # More logical way to require 'qiniu-rs'
-require File.join(File.dirname(__FILE__), 'qiniu', 'conf')
-require File.join(File.dirname(__FILE__), 'qiniu', 'auth', 'digest')
-require File.join(File.dirname(__FILE__), 'qiniu', 'io')
-require File.join(File.dirname(__FILE__), 'qiniu', 'rs')
-require File.join(File.dirname(__FILE__), 'qiniu', 'rs', 'rs')
-require File.join(File.dirname(__FILE__), 'qiniu', 'rs', 'tokens')
-require File.join(File.dirname(__FILE__), 'qiniu', 'fop')
+require 'qiniu/conf'
+require 'qiniu/auth/digest'
+require 'qiniu/io'
+require 'qiniu/rs'
+require 'qiniu/rs/rs'
+require 'qiniu/rs/tokens'
+require 'qiniu/fop'
