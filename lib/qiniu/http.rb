0846acef19b8eab05a0e9b82bcaa2e1e761e558f
Howl王 <howl.wong@gmail.com>
2015-02-16 18:23:07 +0800

    try fix req_headers bugs

--- lib/qiniu/http.rb
+++ lib/qiniu/http.rb
@@ -86,6 +86,7 @@ module Qiniu
         if opts[:headers].is_a?(Hash) then
           req_headers.merge!(opts[:headers])
         end
+        debugger
         ### 发送请求
         response = connection.post(url, req_body, req_headers)        
         return response.status, response.body, response.headers
@@ -125,9 +126,13 @@ module Qiniu
       end # api_post
 
       def management_post (url, body = '')
+        debugger
         ### 授权并执行管理操作
         return HTTP.api_post(url, body, {
-          :headers => { 'Authorization' => 'QBox ' + Auth.generate_acctoken(url, body) }
+          :headers => { 
+            'authorization' => 'QBox ' + Auth.generate_acctoken(url, body),
+            :content_type => 'application/x-www-form-urlencoded'
+          }
         })
       end # management_post
       private # 私有
