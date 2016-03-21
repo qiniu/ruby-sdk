f00f8a234488c3797ce9d09197b477e98d7168da
AlunYou <xianlun.you@autodesk.com>
2016-03-21 20:41:36 +0800

    restore

--- lib/qiniu/http.rb
+++ lib/qiniu/http.rb
@@ -7,7 +7,7 @@ module Qiniu
     class << self
       public
       def is_response_ok?(http_code)
-          return 200 <= http_code && http_code <= 299
+        return 200 <= http_code && http_code <= 299
       end # is_response_ok?
 
       def generate_query_string(params)
@@ -22,9 +22,9 @@ module Qiniu
       def get (url, opts = {})
         ### 配置请求Header
         req_headers = {
-          :connection => 'close',
-          :accept     => '*/*',
-          :user_agent => Config.settings[:user_agent]
+            :connection => 'close',
+            :accept     => '*/*',
+            :user_agent => Config.settings[:user_agent]
         }
 
         # 优先使用外部Header，覆盖任何特定Header
@@ -48,7 +48,7 @@ module Qiniu
       def api_get (url, opts = {})
         ### 配置请求Header
         headers = {
-          :accept => API_RESULT_MIMETYPE
+            :accept => API_RESULT_MIMETYPE
         }
 
         # 将特定Header混入外部Header中
@@ -73,12 +73,12 @@ module Qiniu
         return resp_code, resp_body, resp_headers
       end # api_get
 
-      def post (url, req_body = nil, opts = {}, keep_alive_context = nil)
+      def post (url, req_body = nil, opts = {})
         ### 配置请求Header
         req_headers = {
-          :connection => 'close',
-          :accept     => '*/*',
-          :user_agent => Config.settings[:user_agent]
+            :connection => 'close',
+            :accept     => '*/*',
+            :user_agent => Config.settings[:user_agent]
         }
 
         # 优先使用外部Header，覆盖任何特定Header
@@ -87,8 +87,7 @@ module Qiniu
         end
 
         ### 发送请求
-        response = keep_alive_context.execute(:method => :post, :url => url,
-                                             :payload => req_body, :headers => req_headers)
+        response = RestClient.post(url, req_body, req_headers)
         return response.code.to_i, response.body, response.raw_headers
       rescue => e
         Log.logger.warn "#{e.message} => Qiniu::HTTP.post('#{url}')"
@@ -98,10 +97,10 @@ module Qiniu
         return nil, nil, nil
       end # post
 
-      def api_post (url, req_body = nil, opts = {}, keep_alive_context = nil)
+      def api_post (url, req_body = nil, opts = {})
         ### 配置请求Header
         headers = {
-          :accept => API_RESULT_MIMETYPE
+            :accept => API_RESULT_MIMETYPE
         }
 
         # 将特定Header混入外部Header中
@@ -112,7 +111,7 @@ module Qiniu
         end
 
         ### 发送请求，然后转换返回值
-        resp_code, resp_body, resp_headers = post(url, req_body, opts, keep_alive_context)
+        resp_code, resp_body, resp_headers = post(url, req_body, opts)
         if resp_code.nil? then
           return 0, {}, {}
         end
@@ -129,8 +128,8 @@ module Qiniu
       def management_post (url, body = '')
         ### 授权并执行管理操作
         return HTTP.api_post(url, body, {
-          :headers => { 'Authorization' => 'QBox ' + Auth.generate_acctoken(url, body) }
-        })
+                                    :headers => { 'Authorization' => 'QBox ' + Auth.generate_acctoken(url, body) }
+                                })
       end # management_post
     end # class << self
 
