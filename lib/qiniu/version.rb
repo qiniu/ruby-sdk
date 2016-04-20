ba17910c67786d67a52e08ecb59fcdf876e98cef
Yuan Ping <yp.xjgz@gmail.com>
2016-04-20 16:03:13 +0800

    Update Version

--- lib/qiniu/version.rb
+++ lib/qiniu/version.rb
@@ -1,17 +1,5 @@
 # -*- encoding: utf-8 -*-
 
 module Qiniu
-    module Version
-      MAJOR = 6
-      MINOR = 5
-      PATCH = 2
-      # Returns a version string by joining <tt>MAJOR</tt>, <tt>MINOR</tt>, and <tt>PATCH</tt> with <tt>'.'</tt>
-      #
-      # Example
-      #
-      #   Version.to_s # '1.0.2'
-      def self.to_s
-        [MAJOR, MINOR, PATCH].join('.')
-      end
-    end # Version
+  VERSION = "6.5.1"
 end # module Qiniu
