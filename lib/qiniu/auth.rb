42f4e29dc4a8cba1ba40b1d8b059f72eec83d536
Leon Xu <xfstart07@gmail.com>
2017-01-10 23:02:28 +0800

    URL字符串转义方法更换, 使用URI.escape
    URL字符串转义方法更换, 使用URI.escape

--- lib/qiniu/auth.rb
+++ lib/qiniu/auth.rb
@@ -3,7 +3,6 @@
 
 require 'hmac-sha1'
 require 'uri'
-require 'cgi'
 
 require 'qiniu/exceptions'
 
@@ -205,7 +204,7 @@ module Qiniu
 
         ### 对包含中文或其它 utf-8 字符的 Key 做下载授权
         def authorize_download_url_2(domain, key, args = EMPTY_ARGS)
-          url_encoded_key = CGI::escape(key)
+          url_encoded_key = URI.escape(key)
 
           schema = args[:schema] || "http"
           port   = args[:port]
