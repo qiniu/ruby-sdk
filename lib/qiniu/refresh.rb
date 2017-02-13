7261177d98ab498bcc262fe9b6c7e405642e22db
leon <leon@leons-MacBook-Pro.local>
2017-02-13 16:50:42 +0800

    fix

--- lib/qiniu/refresh.rb
+++ lib/qiniu/refresh.rb
@@ -5,15 +5,16 @@ module Qiniu
 
   module Refresh
     
-    def post urls=[], dirs=[]
-      Net::HTTP.post URI('http://fusion.qiniuapi.com/v2/tune/refresh'), {"urls" => urls, "dirs" => dirs}.to_json, "Authorization" => "#{Config.settings[:access_key]}:#{access_token}", "Content-Type" => "application/json"
-    end
+    class << self
+      def post urls=[], dirs=[]
+        Net::HTTP.post URI('http://fusion.qiniuapi.com/v2/tune/refresh'), {"urls" => urls, "dirs" => dirs}.to_json, "Authorization" => "#{Config.settings[:access_key]}:#{access_token}", "Content-Type" => "application/json"
+      end
     
-    private
-    def access_token
-      `echo "/v2/tune/refresh" |openssl dgst -binary -hmac "#{Config.settings[:secret_key]}" -sha1 |base64 | tr + - | tr / _`
+      private
+      def access_token
+        `echo "/v2/tune/refresh" |openssl dgst -binary -hmac "#{Config.settings[:secret_key]}" -sha1 |base64 | tr + - | tr / _`
+      end
     end
-    
   end
 
 end
